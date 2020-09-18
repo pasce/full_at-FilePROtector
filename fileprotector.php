9839f1cb90254b6ce33626a6ec110d67370238f5
pasce <pasce00@hotmail.com>
2020-09-18 14:00:39 +0200

    Added index.php

--- fileprotector.php
+++ fileprotector.php
@@ -3,12 +3,17 @@
  * @package FilePROtectorPlugin
  */
 /*
-Plugin Name: File PROtector WP PLUGIN
+Plugin Name: File PROtector
 Plugin URI: https://www.full.at/wp-plugins/fileprotector
-Description: Wordpress Plugin to protect assets  from direct access.
+Description: Wordpress Plugin to protect assets from direct access.
 Version: 1.0
 Author: Pascal Werkl
 Author URI: https://www.full.at/pasce
-License: A "Slug" license name e.g. GPL2
+License: GPL2v2 or later
 Text Domain: fileprotector
 */
+
+/*
+ADD LICENSE
+
+ */
