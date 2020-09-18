cc94406c5fcb3d9db7e95daddb78527fc828f933
pasce <pasce00@hotmail.com>
2020-09-18 12:13:09 +0200

    Initial commit

---
+++ FilePROtector.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: File PROtector WP PLUGIN
+Plugin URI: https://www.full.at/wp-plugins/fileprotector
+Description: Wordpress Plugin to protect assets  from direct access.
+Version: 1.0
+Author: Pascal Werkl
+Author URI: https://www.full.at/pasce
+License: A "Slug" license name e.g. GPL2
+*/
